524ff9ea32f97de0421cd0d765aee74697bb5c71
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2025-12-23 21:45:26 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Hello World")
+print("bye")
+#nothing 
+number=1
+unit= 2
+digit= 3
+gender= "Male"
+print(gender)
+print(unit+digit)
+print(unit*digit)
+print(number)
+print(unit+digit*2)
+print(unit+digit-1)
+print('She was about to slip on her toes, but her cousin\'s mother held her')
+#PDP is another political part like BJP and INC and is politically active in Jammu and Kashimr area only.
+x= "PDP is another political part like BJP and INC and is politically active in Jammu and Kashimr area only."
+print(x)
+print(type{x})
