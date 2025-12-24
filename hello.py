a45788d558e7a01ed5b31b731eb839a8889e0ffa
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2025-12-24 20:48:49 +0530

    version 2

--- hello.py
+++ hello.py
@@ -11,8 +11,42 @@ print(unit*digit)
 print(number)
 print(unit+digit*2)
 print(unit+digit-1)
+print(unit+digit-2)
 print('She was about to slip on her toes, but her cousin\'s mother held her')
 #PDP is another political part like BJP and INC and is politically active in Jammu and Kashimr area only.
 x= "PDP is another political part like BJP and INC and is politically active in Jammu and Kashimr area only."
+x= "PDP is another political part like BJP and INC and is politically active in Jammu and Kashimr area only."
 print(x)
-print(type{x})
+print("hel\np") # to follow in a new line
+print("hel\p")  # for having a back slash in the line
+print("hel\tp") # for gap
+city= "Indore"
+temp=45
+weatherreport= "The tempreature in "+ city + " is " + str(temp) + " degrees "
+print(weatherreport) # for keeping the gap give space in the quotes as given in the words IS and DEGREES
+weatherreport= f"The tempreature in {city} is {temp} degrees "
+print(weatherreport)
+# this is concatenation
+SCHOOL= "home"
+print(SCHOOL)
+print(f'This is a curly bracket:}}') # one curly bracket was to get ignored and other was to print
+
+a=" Python "
+res= (a * 3)
+print(res)
+
+star= '*'
+print((star * 5 + "\n") * 5) 
+star= ' * '
+print((star * 5 + "\n") * 5) 
+
+A= "Progression"
+len= len(A)        #to get the length of the word
+print(len)
+
+print(42>21)
+print ("apple"<"chiku")
+print(2==4)
+
+print(ord('a'))
+print(ord("j"))
