a12920e0ec726fb219120825edbb9e446d10ddd6
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2026-01-19 20:15:53 +0530

    added codes

---
+++ If else, elif/control flow.py
@@ -0,0 +1,45 @@
+
+'''weight = int(input(18))
+if weight >= 15:
+    print("Hello Arvind")
+    print("sit here")
+else : print("Hey unknown")'''
+
+
+
+# height = int(input())
+# if height >= 165:
+#    print('you are tall')
+# else : print('drink more milk')
+
+# temp = 15175
+
+# is_snowing = True
+
+# if temp < 10:
+#     print('It is freezing')
+#     print('Wear a heavy coat')
+#     if is_snowing:
+#      print('And don\'t forget your boots!')
+# print('Have a nice day!')
+
+# WHILE LOOP
+# (My code)
+# x = 1
+# while x <= 500:
+#     print("Radhe Radhe", x)
+#     x += 1
+#     if x == 252:
+#         break
+
+
+# (gpt code)
+# x = 1
+# while x <= 500:
+#     if x == 252:
+#         break
+#     print("Radhe Radhe", x)
+#     x += 1
+
+
+
---
+++ syntax.py.py
@@ -0,0 +1,23 @@
+"""                                                                           SYNTAX
+                                                                  Rules to write python program  
+  
+ 1. FUNCTIONS in python= Helps to print on console
+                         Example= print(), user(), etc.
+                         https://docs.python.org/3/library/functions.html
+
+
+ 2. PARENTHESES/ BRACKET= Put bracket after every function 
+                         Example- print("Happy")
+
+ 
+ 3. STRING= Sequence of Unicode characters must be accompanied with quotes 
+         Either this "k" or 'k' 
+
+ 
+ 4. INDENTATION= Space in the begining of line
+                 Do not put unnecessary space in starting of lines also (4 space= Tab)
+
+ 
+ 5. COMMENT= using "#" = To get the line ignored / want to not to run by python
+                         or can also put Triple Quotes '''hope''' double quotes """
+
