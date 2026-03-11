77f2356361329334ff3a2f4caa001c696a26cbf4
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2026-03-11 21:05:03 +0530

    strings

---
+++ _06_Strings/rough.py
@@ -0,0 +1,46 @@
+a="14"
+b="25"
+print(a+" " +b)
+
+print("14"+str(2))
+#txt=   '' p     y     t    h       o     n        p   r   o   g  r    a  m    m   i    n   g  ''
+     #    [0]     [1]   [2]   [3]   [4]   [5]    [6]     7   8   9  10  11  12  13  14   15   16  17
+      #   -18     -17  -16   -15  -14   -13    -12    -11 -10  -9  -8  -7  -6  -5  -4  -3  -2  -1 
+
+txt= 'python programming'
+# string[start:end:steps]
+print(txt[:6:2])
+print(txt[:6:3])
+print(txt[::-2])
+print(txt[::-1])
+txt = "python is amazing, python is fun"
+print(txt.find('is',17))
+
+player = True
+print(type(player))
+a = '1'
+b = '2'
+c = '3'
+print(a,b,c)
+# print(str(a/b/c))
+print(a+b+c)# print( a;b;c)
+print(a,b,c, sep='|')
+d, e, f = 2, 4, 5
+print(d+e+f)
+
+word = "Photosynthesis"
+print(word[7])
+print(word[0])
+
+name1 = 'pawan'
+name2 = 'kavita'
+print(name1 != name2)
+
+dialogue = 'america\'s economy'
+print(dialogue)
+
+sentence = "you,don't\nknow"
+             
+print(sentence)
+
+quote = 'people with the name starting with letter + [K] + is been called by teacher.'
