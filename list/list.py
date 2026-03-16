de60120d8d7b1167a66fe1b40e5bc56c676fea7f
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2026-03-16 16:10:21 +0530

    added more details

--- list/list.py
+++ list/list.py
@@ -12,3 +12,39 @@ fruits = ["apple", "banana", "mango"]
 fruits2 = ("mango" , "kiwi")
 fruits.append(fruits2)    # here in 'append' you are inserting fruits2 as a whole new list ['apple', 'banana', 'mango', ('mango', 'kiwi')]
 print(fruits)
+
+
+res = []
+for i in range (1, 11):
+    res.append(i ** 2)
+print(res)
+
+
+text = "Python list comprehension is powerful and consise"
+if len(i) in text > 5:
+    print(i.upper)
+
+
+tree = ['Pepal', 'Neem', 'Banyan', 'Banana', 'Apple']
+x = 'Banana'
+idx = 0 
+for name in tree:
+    if (name == x):
+        print(x, "at index" idx)
+        break
+    idx += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
