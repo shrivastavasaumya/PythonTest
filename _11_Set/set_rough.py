5848c3cfd81caadc8a93cfbca39dd7eb60cb339d
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2026-03-25 22:07:08 +0530

    sets completed

---
+++ _11_Set/set_rough.py
@@ -0,0 +1,67 @@
+# my_set = {1, 2, 3}
+# my_set.add("abc")
+# print(my_set) # Output - {1, 2, 3, 4}
+
+# my_set = {1, 2, 3}
+# my_set.update("abc")
+# print(my_set)
+
+
+# ml = {1, 2, 3, 'a',' b', 'c'}
+# # ml.remove(4)
+# # print(ml)            ml.remove(4)          KeyError: 4
+# #                      ~~~~~~~~~^^^
+# # 
+# # ml.discard(4)
+# # print(ml)                {1, 2, 3, 'a', 'c', ' b'}
+
+# print(ml.pop())
+# print(ml)                    b  {1, 2, 3, 'c', 'a'}
+
+
+
+# A = {1, 2, 3}
+# B = {3, 4, 5}
+
+# print("Union:", A | B)
+# print("Intersection:", A & B)
+# print("Difference:", A - B)
+# print("Symmetric Difference:", A ^ B)
+
+
+# A = {1, 2, 3}
+# B = {3, 4, 5}
+
+# A.update(B)
+# print("After update:", A)
+
+# A.intersection_update(B)
+# print("After intersection_update:", A)
+
+# A.difference_update(B)
+# print("After difference_update:", A)
+
+# A.symmetric_difference_update(B)
+# print("After symmetric_difference_update:", A)
+
+
+# A = {1, 2, 3}
+# B = {3, 4, 5}
+
+# A.update(B)
+# print("After update:", A)
+
+# A.intersection_update(B)
+# print("After intersection_update:", A)
+
+# A.difference_update(B)
+# print("After difference_update:", A)
+
+# A.symmetric_difference_update(B)
+# print("After symmetric_difference_update:", A)
+
+set_new = { 1, 2, 3, 5, 9, 8, 7, 10, 11}
+for i in set_new:
+    print(i)
+
+
