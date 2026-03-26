f96f04335427fd9017843d9982b9b0703dafc771
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2026-03-26 21:21:58 +0530

    changes done in functions notes

---
+++ function.rough.py
@@ -0,0 +1,41 @@
+def values(a,b):
+    print((sum([a,b])**2))
+    
+# or this way
+
+def sukm(a,b):
+    return(a+b)**2
+#____________________________________
+
+# def hill(k,j):
+#     if j == 0:
+#         return('not defined')
+#     else:
+#         y = ([k/j])
+#         return(y)
+   
+# print(hill(8,9))
+ 
+ #or
+
+# def kol(k,j):
+#     if j != 0:
+#         return k / j
+    
+
+# print(kol(8,0))
+# print(kol(2,4))
+
+#____________________________________
+
+# def hill(k,j):
+#     if j == 0:
+#         print('not defined')
+#     else:
+#         y = ([k/j])
+#         print(y)
+#     return 
+
+# hill(8,9)
+
+
