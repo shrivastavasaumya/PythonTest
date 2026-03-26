f01128d1339a3bc989b17b76016911b5c94137ea
Saumya Shrivastava <saumya.shrivastava21@gmail.com>
2026-03-26 18:18:55 +0530

    Tuples updated

---
+++ Dictionaries/dict_rough.py
@@ -0,0 +1,13 @@
+
+fruits = {
+"apple": "red",
+"chiku": "brown",
+"Kiwi": "green"
+}
+
+info = {
+    "name": "shubman",
+    "marks": 9.2,
+    "subjects": ["english", "hindi"],
+    3.14: "PI" 
+}
---
+++ _12_Tuple/tuple.py
@@ -0,0 +1,23 @@
+
+# h = (1, 2, 8, 9, 10)
+# k = 7,
+# print(h + k )
+
+# grt = tuple(['ana ', 'bana', 'tin' ])
+# print(type(grt))
+
+# grt = 'kana'[1]
+# print(grt)
+
+
+colours = ('red', 'green', 'blue')
+for x ,n in enumerate(colours, start = 1):
+    print(x,n) 
+
+
+
+# rows = 10
+# cols = 10
+#                              #matrix / pattern format                           
+# for i in range(cols):
+#     print("wow " * rows, i)
